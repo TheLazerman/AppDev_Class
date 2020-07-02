e54bed96be72903425c2e60bae1d3fe29b897d59
Gabe Coones <lazerman595@gmail.com>
2020-07-02 12:29:24 -0500

    Adding core functionality

--- rockPaperScissors/src/rockPaperScissors/Game.java
+++ rockPaperScissors/src/rockPaperScissors/Game.java
@@ -1,5 +1,43 @@
 package rockPaperScissors;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Game { //POJO - Plain ole Java Object
+
+	private String input;
+	private List<String> responses;
+	
+	public void rps() {
+		this.input = null;
+		responses = new ArrayList<String>() {{ add("Rock"); add("Papper"); add("Scissors");}};
+		
+		
+		
+	}
+	
+	
+	public String getInput() {
+		return this.input;
+	}
+
+
+	public boolean prompt(String input) {
+		if(input instanceof String) {
+			this.input = input;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	public String shoot() {
+		return "Rock";
+	}
+	
+	
 
 }
--- rockPaperScissors/test/rockPaperScissors/GameTest.java
+++ rockPaperScissors/test/rockPaperScissors/GameTest.java
@@ -3,17 +3,45 @@ package rockPaperScissors;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GameTest {
+	
+	private static Game rps;
+
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		rps = new Game();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Order(1)
+	void rps_GivenNoParam_ConstructsObj() {
+		assertTrue(rps instanceof Game);
+	}
+	
+	@Test
+	@Order(2)
+	void promptChoice_GivenString_ReturnsTrue() {
+		boolean sucess = false;
+		String input = "rock";
+		sucess = rps.prompt(input);
+		assertTrue(sucess);
+		assertEquals(input, rps.getInput());
+	}
+	
+	@Test
+	@Order(3)
+	void shoot_GivenNoParam_ReturnsResponse() {
+		String response = null;
+		response = rps.shoot();
+		assertEquals("Rock", response);
 	}
 
 }
