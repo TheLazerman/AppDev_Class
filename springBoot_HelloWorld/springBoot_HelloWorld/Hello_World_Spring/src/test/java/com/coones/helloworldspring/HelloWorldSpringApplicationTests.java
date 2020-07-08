556ffab355d68e7a1c4fe1437cb625616ebc49ec
Gabe Coones <lazerman595@gmail.com>
2020-07-08 12:27:13 -0500

    Initial commit

---
+++ springBoot_HelloWorld/springBoot_HelloWorld/Hello_World_Spring/src/test/java/com/coones/helloworldspring/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coones.helloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
